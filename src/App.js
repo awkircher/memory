fced8b2b480c98bae718a98226d1dc49a18cb634
Ashley Kircher <ashleywkircher@gmail.com>
2020-12-15 15:51:22 -0600

    Added links to fonts and title elements

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img className="logo" src="./images/logo.svg" alt="Memory logo"/>
+      <p className="instructions">
+        How's your eagle eye? Try to click every card without clicking the same one twice.
+      </p>
+      <img className="divider" src="./images/divider.svg" alt="Divider line" />
       <Gameboard 
         cards={Data()} />
     </div>
