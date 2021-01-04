17040871bd49c6832fe9ddcecbbb3ccd46d5895d
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-04 11:57:08 -0600

    Added comments about what Data() returns, and how handleCardClick works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Message from './Components/Message'
 import divider from './images/divider.svg'
 
 function App() {
-  const [clicked, setClicked] = useState([]);
+  const [clicked, setClicked] = useState([]); //keeps track of every card you've clicked this game
   const [score, setScore] = useState(0);
   const [pastScores, setPastScores] = useState([]);
   const [topScore, setTopScore] = useState(0);
@@ -21,7 +21,7 @@ function App() {
     return array;
   }
 
-  const cards = shuffleArray(Data());
+  const cards = shuffleArray(Data()); //Data() returns an array of objects, each with an id property and a src property
   
   const calculateTopScore = function() {
     const reducer = (accumulator, currentValue) => {
@@ -34,16 +34,20 @@ function App() {
     return pastScores.reduce(reducer, score);
   }
 
+  //Compares the passed id with the Card id properties already contained in state. 
+  //If there's a match (i.e., you've already clicked this card), then the game ends. 
+  //Otherwise, the passed id is compared with all the Card id properties in Data, 
+  //and the matching Card object is added to state.
   const handleCardClick = function(id) {
     const isCard = (card) => card.id === id;
     const indexInState = clicked.findIndex(isCard);
-    if (indexInState === -1) {
+    if (indexInState === -1) { //keep playing!
       const indexInCardsData = cards.findIndex(isCard);
       setClicked([...clicked, cards[indexInCardsData]]);
       setScore(score + 1);
-    } else {
+    } else { //game over :(
       setPastScores([...pastScores, score]);
-      setTopScore(calculateTopScore())
+      setTopScore(calculateTopScore());
       setScore(0);
       setClicked([]);
     }
